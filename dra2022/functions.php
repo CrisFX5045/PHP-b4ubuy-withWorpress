d7c34ec6cb34967127247ba357a0aae57ea43db9
CrisFX5045 <70766389+CrisFX5045@users.noreply.github.com>
2022-05-01 16:20:13 -0600

    Machote inicial

---
+++ dra2022/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+    function load_assets(){
+
+        //Permite cargar el BS CSS
+        wp_enqueue_style('bootstrap_css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
+        
+        //Cargar BS JS
+
+        wp_enqueue_script('bootstrap_js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js');
+
+        //permite cargar el csss del tema
+        wp_enqueue_style('style', get_stylesheet_uri() );
+
+    }
+
+    add_action('wp_enqueue_scripts', 'load_assets');
+
+
+    //Soporte para imagenes destacadas en paginas
+
+    add_theme_support('post-thumbnails');
+
+?>
