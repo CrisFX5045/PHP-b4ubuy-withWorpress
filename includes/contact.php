5a60e1e34aa016693cc236d1874dd3f79c6cdc35
CrisFX5045 <70766389+CrisFX5045@users.noreply.github.com>
2022-05-01 16:21:40 -0600

    Machote inicial

---
+++ includes/contact.php
@@ -0,0 +1,21 @@
+<!-- contact -->
+
+<div class="col-5 justify-content-center">
+    <div class="row">
+        <h2>Contact</h2>
+    </div>
+    <div class="row">
+        <label for="">Full name</label>
+        <input type="text">
+    </div>
+    <div class="row">
+        <label for="">Email</label>
+        <input type="text">
+    </div>
+    <div class="row">
+        <input type="submit" value="Message">
+    </div>
+</div>
+
+
+<!-- contact -->
