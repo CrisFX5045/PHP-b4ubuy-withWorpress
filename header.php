a9eb2ea20d1c673ee1c03cbcd9a0773fd2e2fd01
CrisFX5045 <70766389+CrisFX5045@users.noreply.github.com>
2022-05-01 20:12:59 -0600

    prueba 2

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
    <!-- container -->
     <div>
         <!-- header -->
-        <header>
+     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">Navbar</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -65,5 +65,5 @@
             </a>
         </div>
     
-        </header>
+     </header>
         <!-- header -->
