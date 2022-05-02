b0e5de4e0d42e1df8933f921d33cc8ba8cf35c9e
Jostin24 <tsukaima242010@gmail.com>
2022-05-01 18:53:13 -0600

    Prueba virtual

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php get_header(); ?>
 
-<h1>Probando el repositorio con SFTP si funciona</h1>
 
-<!-- content -->
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab molestiae repellendus illum consequatur atque cumque ea eos voluptates minus ipsum! Consequuntur quam esse fuga eius nostrum ullam in quaerat provident!</p>
-<!-- content -->  
 
 <?php get_footer(); ?>
