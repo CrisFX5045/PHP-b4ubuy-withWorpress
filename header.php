f72d4ee3836d65b30d50f653c52014103ded1939
Jostin24 <tsukaima242010@gmail.com>
2022-07-11 16:57:43 -0600

    Update header.php
    header

--- header.php
+++ header.php
@@ -25,21 +25,19 @@
       
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         
-            <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="reviews">Reviews</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="news">News</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="services">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                         <a class="nav-link" href="#">ES</a>
-                    </li>
-                 
-            </ul>
+      <?php
+     wp_nav_menu( array(
+        'theme_location'  => 'primary',
+        'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+        'container'       => 'div',
+        'container_class' => 'collapse navbar-collapse',
+        'container_id'    => 'bs-example-navbar-collapse-1',
+        'menu_class'      => 'navbar-nav mr-auto',
+        'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+        'walker'          => new WP_Bootstrap_Navwalker(),
+    ) );
+     
+     ?>
        
       </div> 
     </div>
